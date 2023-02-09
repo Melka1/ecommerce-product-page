e49d571d16e13c501ce5f9d76decc7b50f21942c
Melka Tolessa <melkatole1@gmail.com>
2023-02-09 23:57:41 +0300

    some new updates

--- scripts.js
+++ scripts.js
@@ -80,17 +80,44 @@ function handleDelete(id){
     toggleCart()
 }
 
+function handleNext(){
+    image++;
+    if(image==5) image = 1
+    $("aside .preview--container img").attr("src", `./images/image-product-${image}.jpg`)
+    $(`aside .prev div`).css("border", "initial")
+    $(`aside .prev div img`).css("opacity", "initial")
+    $(`aside .thumbnail${image} div`).css("border", "2px solid hsl(26, 100%, 55%)")
+    $(`aside .thumbnail${image} div img`).css("opacity", "0.5")
+}
+
+function handlePrev(){
+    image--;
+    if(image==0) image = 4
+    $("aside .preview--container img").attr("src", `./images/image-product-${image}.jpg`)
+    $(`aside .prev div`).css("border", "initial")
+    $(`aside .prev div img`).css("opacity", "initial")
+    $(`aside .thumbnail${image} div`).css("border", "2px solid hsl(26, 100%, 55%)")
+    $(`aside .thumbnail${image} div img`).css("opacity", "0.5")
+}
+
+
 function openPreview() {
     $("body").append(`
     <aside>
     <div class="product--preview">
       <div class="background"></div>
       <div class="preview">
-        <div class="img--close">
-          <img src="./images/icon-close.svg" alt=""  onclick="closePreview()">
+        <div class="img--close" onclick="closePreview()">
+          <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="hsl(26, 100%, 55%)" fill-rule="evenodd"/></svg>
         </div>
         <div class="preview--container">
-          <img src="./images/image-product-${image}.jpg" alt="">
+            <button class="prev" onclick="handlePrev()">
+            <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M11 1 3 9l8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>
+            </button>
+            <img src="./images/image-product-${image}.jpg" alt="">
+            <button class="next" onclick="handleNext()">
+            <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>
+            </button>
         </div>
         <div class="thumbnail1 prev">
             <div>
